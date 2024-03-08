83bc69a65769a882e585eaa8f192f4276c743c4c
Mariam Dib <mariame.dibaga@gmail.com>
2024-03-08 16:23:47 +0100

    Affichage modale2

--- FrontEnd/JS/Index.js
+++ FrontEnd/JS/Index.js
@@ -75,6 +75,7 @@ async function displayWorks(categorieId) {
     gallery.appendChild(workContainer);
   }  
   });
+  
 }
 
 // Fonction pour créer les boutons de filtre et gérer les événements
@@ -146,6 +147,16 @@ function modifyHomePageForAdmin() {
  });
  }
 
+async function displayModalWorks() {
+  const gallery = document.querySelector(".galleryModal");
+  gallery.innerHTML = "";
+  works = await fetchWorks();
+  works.forEach((work)=>{
+    const img = document.createElement("img");
+    img.src = work.imageUrl;
+    gallery.appendChild(img);
+  });
+}
 
 function displayModal() {
   // Afficher la modal au milieu
@@ -166,17 +177,83 @@ function displayModal() {
   aside.append(modalTitle, closeIcon, gallery, borderLine, btnAdd);
   containerModal.appendChild(aside);
   document.body.appendChild(containerModal);
-  // Appeler la fonction pour afficher les travaux
-  displayWorks().then(works => {
-    works.forEach(work => {
-      //Afficher chaque image sans titre
-      const img = document.createElement("img");
-      img.src = work.imageUrl;
-      gallery.appendChild(img);
-    });
-  });
+
+  
+  //Appeler la fonction pour afficher les travaux//
+  displayModalWorks();
+
+  btnAdd.addEventListener("click", function() {
+    // Appel de la fonction pour afficher la deuxième modal
+    displayModal2();
+    // Masquer la modal actuelle
+    aside.style.display = "none"; // pour masquer la modal displayModal
+ });
  }
 
+
+ function displayModal2() {
+  // Afficher la modal au milieu
+const containerModal = document.createElement("div");
+containerModal.classList.add("containerModal");
+const aside = document.createElement("aside");
+aside.classList.add("modifyWork");
+const modalTitle2 = document.createElement("h3");
+modalTitle2.innerHTML = "Ajout photo";
+const closeIcon = document.createElement("i");
+closeIcon.classList.add("fa-solid", "fa-xmark", "close");
+const arrowIcon = document.createElement("i");
+arrowIcon.classList.add("fa-solid", "fa-arrow-left", "arrowReturn");
+
+const formElement = document.createElement('div');
+formElement.classList.add('form');
+formElement.setAttribute('enctype', 'multipart/form-data');
+formElement.setAttribute('action', '#');
+formElement.setAttribute('method', 'post');
+const gallery2 = document.createElement("div");
+gallery2.classList.add("ajouterPhoto");
+const iconElement = document.createElement('i');
+iconElement.classList.add('fa', 'fa-regular', 'fa-image', 'fa-5x');
+const labelElement = document.createElement('label');
+labelElement.id = 'btnAddPicture';
+labelElement.setAttribute('for', 'filePicture');
+labelElement.textContent = '+ Ajouter photo';
+const inputElement = document.createElement('input');
+inputElement.type = 'file';
+inputElement.name = 'image';
+inputElement.id = 'filePicture';
+inputElement.multiple = true;
+inputElement.accept = 'images/*';
+inputElement.required = true;
+const paragraphElement = document.createElement('p');
+paragraphElement.textContent = 'jpg, png : 4mo max';
+const addPhoto = document.getElementById("ajouterPhoto");
+
+const borderLine = document.createElement("hr");
+const btnValider = document.createElement("div");
+btnValider.innerHTML = "Valider";
+btnValider.id = "btnSubmit";
+aside.append(modalTitle2, closeIcon, arrowIcon, formElement, borderLine, btnValider);
+containerModal.appendChild(aside);
+document.body.appendChild(containerModal);
+addPhoto.appendChild(iconElement);
+addPhoto.appendChild(labelElement);
+addPhoto.appendChild(inputElement);
+addPhoto.appendChild(paragraphElement);
+formElement.appendChild(gallery2);
+formElement.appendChild(iconElement);
+formElement.appendChild(labelElement);
+formElement.appendChild(inputElement);
+formElement.appendChild(paragraphElement);
+
+ // Code pour afficher la modale au millieu de la page
+ containerModal.style.display = "block";
+ containerModal.style.position = "fixed";
+ containerModal.style.top = "50%";
+ containerModal.style.left = "50%";
+ containerModal.style.transform = "translate(-50%, -50%)";
+ // la modal précédente est masquée
+ aside.style.display = "none";
+}
 
 
 /*
